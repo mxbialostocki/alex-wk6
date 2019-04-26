984e8f6263ab26866e068b04517d37d5bdc9e2ef
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-04-26 13:00:48 +1200

    add cavnas.jsx udpates

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { retrieveOracles } from '../api'
 
 class Canvas extends React.Component {
-  render () {
-    return (
-      <h1>This renders Canvas Component</h1>
-    )
-  }
+oracleDeck = []
+componentDidMount () {
+  retrieveOracles()
+    .then(oracles => {
+      console.log('Canvas.jsx', oracles)
+    })
+}
+
+render () {
+  return (
+    <h1>This renders Canvas Component</h1>
+  )
+}
 }
 
 export default Canvas
