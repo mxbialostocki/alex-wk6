8c0b1f7b45fedeaf4937506b8d9daaf4a3f67b46
Xjana Lamasi Luamanuvae-Su'a <xjana1234@gmail.com>
2019-04-26 14:18:13 +1200

    update Canvas.jsx to ranomise cards

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -2,19 +2,49 @@ import React from 'react'
 import { retrieveOracles } from '../api'
 
 class Canvas extends React.Component {
-oracleDeck = []
-componentDidMount () {
-  retrieveOracles()
-    .then(oracles => {
-      console.log('Canvas.jsx', oracles)
-    })
-}
+  state = {
+    cards: []
+  }
 
-render () {
-  return (
-    <h1>This renders Canvas Component</h1>
-  )
-}
+  componentDidMount () {
+    retrieveOracles()
+      .then(oracles => {
+        console.log(oracles)
+        this.setState({
+          cards: drawThree(oracles)
+        })
+        console.log(this.state.cards)
+      })
+  }
+
+  render () {
+    return (
+      <h1>This renders Canvas Component</h1>
+    )
+  }
 }
 
 export default Canvas
+
+// function drawThree (oracleDeck) {
+//   const drawThree = []
+//   while (drawThree.length < 4) {
+//     drawThree.push(oracleDeck[Math.floor(Math.random() * oracleDeck.length)])
+//   }
+//   return drawThree
+// }
+
+function drawThree (cards) {
+  const selection = []
+
+  for (let i = 0; i < 3; i++) {
+    const randomCard = cards[Math.floor(Math.random() * cards.length)]
+    if (!selection.includes(randomCard)) {
+      selection.push(randomCard)
+    } else {
+      i--
+    }
+  }
+
+  return selection
+}
